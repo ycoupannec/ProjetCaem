e68e880113bf30fbd387d315ba6dfeeb38122849
ycoupannec <yohann.coupannec@gmail.com>
2017-02-15 10:22:35 +0100

    update gender to varchar

---
+++ database/migrations/2017_02_15_091935_update_gender_varchar.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateGenderVarchar extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('people', function (Blueprint $table) {
+            
+            
+            $table->string('gender')->change();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
