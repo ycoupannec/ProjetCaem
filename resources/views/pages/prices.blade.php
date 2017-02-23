a5fd50550c05afb43ed4e44eaabbc227e0a9a459
YetAnotherBen <benoit.m@codeur.online>
2017-02-23 18:16:25 +0100

    before leaving

--- resources/views/pages/prices.blade.php
+++ resources/views/pages/prices.blade.php
@@ -1,19 +1,85 @@
-!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--CALLING THE DEFAULT LAYOUT-->
+@extends('layouts.default')
+<!------------------------------>
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
+<!--HEAD TITLE :-->
+@section('pageTitle')
+CAEM - Tarifs &amp; Inscriptions
+@endsection
+<!---------------->
 
-    </head>
-    <body>
-        <p>price</p>
-    </body>
-</html>
+<!--MAIN CONTENT :-->
+@section('pageContent')
+<main class="mainTarifs container">
+
+      <section class="row">
+
+        <header>
+          <p>Tarifs & inscriptions</p>
+        </header>
+
+        <h2 class="titleTop">L'inscription est possible toute l'année</h2>
+
+        <p class="tarifsIntro">Pré-inscriptions par email ou téléphone. Inscriptions au bureau du CAEM avec Sonia du lundi au jeudi de 9h15 à 16h00. Pour le règlement des activités, nous vous donnons la possibilité de choisir le mode de paiement qui vous convient le mieux : mensualisation en prélèvement automatique sur 12 mois, paiement par chèque en plusieurs fois, chèques vacances...</p>
+
+        <h2>Éveil musical 4 – 6 ans</h2>
+        <ul>
+          <li>Tarif CAEM : environ 9 €/mois (109 € l'année)</li>
+          <li>Tarif maison de quartier : à partir de 27 € l'année</li>
+        </ul>
+
+        <h2>Parcours musical 6 – 11 ans</h2>
+        <ul>
+          <li>30,25 €/mois (363 € l’année)</li>
+        </ul>
+
+        <h2>Cours d’instruments</h2>
+        <ul>
+          <li>Cours en groupe (45 minutes, 2 à 4 personnes selon instrument) : 37,75 €/mois (453 € l’année)</li>
+          <li>Cours individuel (30 minutes) : 45,5€/mois (546 € l’année)</li>
+        </ul>
+
+        <h2>Cours de chant</h2>
+        <ul>
+          <li>30 minutes : 32,5 €/mois (390 € l'année)</li>
+          <li>45 minutes : 48,75 €/mois (585 € l'année)</li>
+        </ul>
+
+        <h2>Ateliers thématiques</h2>
+        <ul>
+          <li>31,25 €/mois (375 € l’année) <div class="disclaimer">- 50 % avec la formule !</div></li>
+        </ul>
+
+        <h2>La Formule - 26 ans</h2>
+        <ul>
+          <li>Cours d’instrument en groupe</li>
+          <li>+ Atelier de groupe* ou atelier thématique (* 1h / à partir de 10 ans / pratique de musiques actuelles avec des jeunes qui jouent d'autres instruments / 4 à 7 personnes)</li>
+          <p class="disclaimer">Tarifs</p>
+          <li>42 €/mois (504 € l'année)</li>
+        </ul>
+
+        <h2>La Formule adulte</h2>
+        <ul>
+          <li>Cours d’instrument en groupe</li>
+          <li>+ Atelier thématique 1h30 ou 2h selon le nombre d'inscrits</li>
+          <p class="disclaimer">Tarifs</p>
+          <li>environ 50 €/mois (selon atelier)</li>
+        </ul>
+
+        <h2>Pratiques collectives & Expression Corporelle</h2>
+        <ul>
+          <li>Batucada 55 € l'année</li>
+          <li>Chorale 12,75 €/mois (153 € l'année) + 15 € forfait partitions</li>
+          <li>Fanfare Groove 5 €/mois (60 € l'année)</li>
+          <li>Taï Chi 10 €/mois (120 € l'année)</li>
+          <li>Danse Hip-hop 10 €/mois (120 € l'année)</li>
+          <p class="disclaimer">- 50 % sur les pratiques collectives et activités d'expression corporelle si vous pratiquez une autre activité au CAEM</p>
+        </ul>
+
+    </section>
+
+    </main>
+@endsection
+<!------------------>
--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -1,19 +1,182 @@
-!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--CALLING THE DEFAULT LAYOUT-->
+@extends('layouts.default')
+<!------------------------------>
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
+<!--HEAD TITLE :-->
+@section('pageTitle')
+CAEM - Mon parcours musical
+@endsection
+<!---------------->
 
-    </head>
-    <body>
-        <p>team</p>
-    </body>
-</html>
+
+
+<!--MAIN CONTENT :-->
+@section('pageContent')
+<main class="container mainEquipe">
+
+        <section id="sectionContributors" class="row">
+
+          <header>
+            <p>Intervenants</p>
+          </header>
+
+            <div class="contribLine col-md-12">
+                  <div class="col-md-3">
+                      <img src="../assets/img/imgtest.png">
+                      <h3>Martin Schiffmann</h3>
+                      <p>Pianiste</p>
+                  </div>
+                  <div class="col-md-3">
+                      <img src="../assets/img/imgtest.png">
+                      <h3>Philippe Bregand</h3>
+                      <p>Guitariste, musicien conseil</p>
+                  </div>
+                  <div class="col-md-3">
+                      <img src="../assets/img/imgtest.png">
+                      <h3>Jean-Christophe David</h3>
+                      <p>Violoniste/altiste</p>
+                  </div>
+                  <div class="col-md-3">
+                      <img src="../assets/img/imgtest.png">
+                      <h3>Jean-Baptiste Chané</h3>
+                      <p>Musicothérapeute</p>
+                  </div>
+            </div>
+
+            <div class="contribLine col-md-12">
+                  <div class="col-md-3">
+                      <img src="../assets/img/imgtest.png">
+                      <h3>Alexandre Joly</h3>
+                      <p>Percussioniste</p>
+                  </div>
+                  <div class="col-md-3">
+                      <img src="../assets/img/imgtest.png">
+                      <h3>Gregory Tran</h3>
+                      <p>Bassiste, pianiste</p>
+                  </div>
+                  <div class="col-md-3">
+                      <img src="../assets/img/imgtest.png">
+                      <h3>Robin Grisinelli</h3>
+                      <p>Guitariste</p>
+                  </div>
+                  <div class="col-md-3">
+                      <img src="../assets/img/imgtest.png">
+                      <h3>Marcjean</h3>
+                      <p>Saxophoniste, chanteur</p>
+                  </div>
+            </div>
+
+            <div class="contribLine col-md-12">
+                <div class="col-md-3">
+                    <img src="../assets/img/imgtest.png">
+                    <h3>Henri Alfred Suzon</h3>
+                    <p>Trompettiste</p>
+                </div>
+                <div class="col-md-3">
+                    <img src="../assets/img/imgtest.png">
+                    <h3>Joseph Barbier</h3>
+                    <p>Batteur</p>
+                </div>
+                <div class="col-md-3">
+                    <img src="../assets/img/imgtest.png">
+                    <h3>Claire David</h3>
+                    <p>Chanteuse, tromboniste</p>
+                </div>
+                <div class="col-md-3">
+                    <img src="../assets/img/imgtest.png">
+                    <h3>Naty Andria</h3>
+                    <p>Chanteuse</p>
+                </div>
+            </div>
+
+            <div class="contribLine col-md-12">
+                <div class="col-md-3">
+                    <img src="../assets/img/imgtest.png">
+                    <h3>Sébastion Goret</h3>
+                    <p>Pianiste, Taï Ji</p>
+                </div>
+                <div class="col-md-3">
+                    <img src="../assets/img/imgtest.png">
+                    <h3>Abdellah Hireche</h3>
+                    <p>Danseur hip-hop</p>
+                </div>
+            </div>
+
+      </section>
+
+
+
+        <section id="sectionAdministration" class="row">
+
+          <header>
+            <p>Administration</p>
+          </header>
+
+          <h2>Salariés d'administration</h2>
+
+          <div class="adminLine col-md-12">
+              <div class="col-md-3">
+                  <img src="../assets/img/imgtest.png">
+                  <h3>Jean-Basptiste Chané</h3>
+                  <p>Directeur</p>
+              </div>
+              <div class="col-md-3">
+                  <img src="../assets/img/imgtest.png">
+                  <h3>Sonia Monnin</h3>
+                  <p>Accueil, secrétariat</p>
+              </div>
+              <div class="col-md-3">
+                  <img src="../assets/img/imgtest.png">
+                  <h3>Robin Grisinelli</h3>
+                  <p>Communication</p>
+              </div>
+          </div>
+
+          <h2>Conseil d'administration</h2>
+
+          <div class="adminLine col-md-12">
+              <div class="col-md-3">
+                  <img src="../assets/img/imgtest.png">
+                  <h3>Yves Tannier</h3>
+                  <p>Président</p>
+              </div>
+              <div class="col-md-3">
+                  <img src="../assets/img/imgtest.png">
+                  <h3>Christine Anaclet</h3>
+                  <p>Vice-présidente</p>
+              </div>
+              <div class="col-md-3">
+                  <img src="../assets/img/imgtest.png">
+                  <h3>Bruno Berchoud</h3>
+                  <p>Secrétaire</p>
+              </div>
+              <div class="col-md-3">
+                  <img src="../assets/img/imgtest.png">
+                  <h3>Olivier Pautot</h3>
+                  <p>Secrétaire adjoint</p>
+              </div>
+          </div>
+
+          <div class="adminLine col-md-12">
+              <div class="col-md-3">
+                  <img src="../assets/img/imgtest.png">
+                  <h3>Jean-Luc Donier</h3>
+                  <p>Trésorier</p>
+              </div>
+              <div class="col-md-3">
+                  <img src="../assets/img/imgtest.png">
+                  <h3>Mary Moritz</h3>
+                  <p>Trésorière adjoint</p>
+              </div>
+          </div>
+
+          <p class="col-md-12 adminOthers">Autres membres : Géraldine Gobet-Boillon, François Guillet, Jacqueline Martin, Christelle Mouchet, Claire Pourcelot</p>
+
+      </section>
+
+
+    </main>
+@endsection
+<!------------------>
