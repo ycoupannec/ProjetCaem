1a1631572a2c77e5220a01df871eab6341b09174
YetAnotherBen <benoit.m@codeur.online>
2017-02-23 16:29:29 +0100

    +open graph +twitter card

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,7 +1,5 @@
 <!--GENERAL SCRIPT (BOOTSTRAP AND Cie)-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src={{ asset('js/app.js') }}></script>
-----------------------------------
 
-
-@yield('pageScript');
+@yield('pageScript')
