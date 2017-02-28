016b2ea82c09e1b20d1072ce9aae9d6d2a4a8bd9
YetAnotherBen <benoit.m@codeur.online>
2017-02-28 11:33:20 +0100

    "page équipe" clean (or at least, cleaner)

--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -16,54 +16,88 @@ CAEM - L'équipe
 
 
 	<section class="col-md-12">
+
 		<h2>Intervenants</h2>
+
 		@foreach($professeurs as $professeur)
 		<div class="col-md-3">
 
-				@if($professeur->picture != null)
-				<img src="uploads/{{ $professeur->picture }}">
-				@else
-				<img src="uploads/photo_team/placeholder.png">
-				@endif
-				<h3>{{ $professeur->firstname }} {{ $professeur->name }}</h3>
-				<p>{{ $professeur->instrument }}</p>
+			<article>
+
+				<figure>
+					@if($professeur->picture != null)
+					<img src="uploads/{{ $professeur->picture }}">
+					@else
+					<img src="uploads/photo_team/placeholder.png">
+					@endif
+				</figure>
+
+				<figcaption>
+					<h3>{{ $professeur->firstname }} {{ $professeur->name }}</h3>
+					<p>{{ $professeur->instrument }}</p>
+				</figcaption>
+
+			</article>
 
 		</div>
 		@endforeach
+
 	</section>
 
 
 	<section class="col-md-12">
+
 		<h2>Salariés administration</h2>
+
 		@foreach($bureaux as $bureau)
 		<div class="col-md-3">
-			<div>
-				@if($bureau->picture != null)
-				<img src="uploads/{{ $bureau->picture }}">
-				@else
-				<img src="uploads/photo_team/placeholder.png">
-				@endif
-				<h3>{{ $bureau->firstname }} {{ $bureau->name }}</h3>
-				<p>{{ $bureau->instrument }}</p>
-			</div>
+
+			<article>
+
+				<figure>
+					@if($bureau->picture != null)
+					<img src="uploads/{{ $bureau->picture }}">
+					@else
+					<img src="uploads/photo_team/placeholder.png">
+					@endif
+				</figure>
+
+				<figcaption>
+					<h3>{{ $bureau->firstname }} {{ $bureau->name }}</h3>
+					<p>{{ $bureau->instrument }}</p>
+				</figcaption>
+
+			</article>
+
 		</div>
 		@endforeach
+
 	</section>
 
 
 	<section class="col-md-12">
+
 		<h2>Conseil d'Administration</h2>
+
 		@foreach($administrations as $administration)
 		<div class="col-md-3">
-			<div>
-				@if($administration->picture != null)
-				<img src="uploads/{{ $administration->picture }}">
-				@else
-				<img src="uploads/photo_team/placeholder.png">
-				@endif
-				<h3>{{ $administration->firstname }} {{ $administration->name }}</h3>
-				<p>{{ $administration->instrument }}</p>
-			</div>
+
+			<article>
+				<figure>
+					@if($administration->picture != null)
+					<img src="uploads/{{ $administration->picture }}">
+					@else
+					<img src="uploads/photo_team/placeholder.png">
+					@endif
+				</figure>
+
+				<figcaption>
+					<h3>{{ $administration->firstname }} {{ $administration->name }}</h3>
+					<p>{{ $administration->instrument }}</p>
+				</figcaption>
+
+			</article>
+
 		</div>
 		@endforeach
 	</section>
